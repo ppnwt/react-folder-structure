d5e6df2637aada6a9a4f34dc9d2bf0a3dbc0a2f2
New Panuwat <new_sbh@me.com>
2022-07-21 11:59:33 +0700

    fix app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Get started with react fundamentals
+          Test CI/CD
         </p>
       </header>
     </div>
